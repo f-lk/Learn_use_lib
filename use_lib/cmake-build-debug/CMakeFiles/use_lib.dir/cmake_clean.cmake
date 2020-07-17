34733003a35dec2302a6670ec749c4a284f8f80e
深渊为陵 <53933714+f-lk@users.noreply.github.com>
2020-07-17 22:30:20 +0800

    Add files via upload

---
+++ use_lib/cmake-build-debug/CMakeFiles/use_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_lib.dir/main.cpp.o"
+  "use_lib.pdb"
+  "use_lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/use_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ use_lib/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+
+
+
+int main(int argc , char ** argv)
+{
+    out();
+
+    return 0;
+}
